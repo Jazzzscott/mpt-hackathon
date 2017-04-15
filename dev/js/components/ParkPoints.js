539f9694d8cda2cc075759e29dc27e4a7c3f4404
Robin Choi <robinchoii@gmail.com>
2017-04-15 11:59:35 -0700

    add myparkpoints component

--- dev/js/components/ParkPoints.js
+++ dev/js/components/ParkPoints.js
@@ -3,11 +3,32 @@ import React from 'react'
 class ParkPoints extends React.Component {
   render() {
     return(
-      <div className='container'>
-        <div className='row'>
+      <div className='container parkPointsContainer'>
+        <div className='row parkPointsTopRow'>
           <div className='col-sm-12 parkPoints'>
             <img className='img-responsive img-center' src='/img/MPTH_badge.png' />
             <span className='badge-number col-sm-12 text-center'>12</span>
+          </div>
+        </div>
+
+        <hr className='col-sm-12 hrGreen'/>
+
+        <div className='row parkPoints'>
+          <h3 className='col-sm-12'>Redeem</h3>
+          <div className='col-sm-12'>
+            <label><input type='radio' /> Event Voucher</label>
+          </div>
+          <div className='col-sm-12'>
+            <label><input type='radio' /> Class Discount Code</label>
+          </div>
+          <div className='col-sm-12'>
+            <label><input type='radio' /> Zoo Ticket Discount Code</label>
+          </div>
+          <div className='col-sm-12'>
+            <label><input type='radio' /> Donate My ParkPoints</label>
+          </div>
+          <div className='col-sm-6 col-sm-offset-3'>
+            <button className='btn btn-woohoo form-control'>WooHoo!</button>
           </div>
         </div>
       </div>
