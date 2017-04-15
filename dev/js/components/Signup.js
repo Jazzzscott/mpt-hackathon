85dc9c47377416787ae140fde5f5d8984f88accd
Jazzzscott <jadotscott@hotmail.com>
2017-04-15 09:51:15 -0700

    profile page

--- dev/js/components/Signup.js
+++ dev/js/components/Signup.js
@@ -3,18 +3,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 const Signup = () => (
-  <div className="container-fluid signup">
-    <div className="row">
-      <div className="col-sm-12">
-        <div className="signupForm">
-          <p>Welcome to ParkPoints, an app designed to encourage you to explore your local parks.</p>
-          <input className="input" placeholder="email"></input>
-          <input className="input" placeholder="password"></input>
-          <button>Signup</button>
+  <div className="banner-image">
+    <div className="container-fluid signup">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="signupForm">
+            <p>Welcome to ParkPoints, an app designed to encourage you to explore your local parks.</p>
+            <hr className = "hrGreen"/>
+            <input className="input" placeholder="email"></input>
+            <input className="input" placeholder="password"></input>
+            <a className="btn btn-default btn-lg btn-signup" href="#" role="button">Go</a>
+            <hr className = "hrGreen"/>
+            <p>Earn points every time you check in at a park. Once you''ve earned 50 points, redeem them for a voucher to a Tacoma Metro Park event! </p>
+          </div>
         </div>
-      </div>
-    </div>    
-   </div>
+      </div>    
+    </div>
+  </div>  
 );
 
 export default Signup;
