0e68d93d3951e677d3acc2d462f13e1bfe32f6a4
Robin Choi <robinchoii@gmail.com>
2017-04-14 19:12:10 -0700

    unfinished setting and event page

---
+++ dev/js/components/Event.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Event extends React.Component{
+  render(){
+    return(
+    <div>
+      <div className="event">
+        HOLD OFF FOR NOW
+      </div>
+    </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Event />,document.getElementById('app'));
