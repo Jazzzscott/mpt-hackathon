48cebb98ded7de7df1c68bd3f9c189c55fa0873e
Andrew Dickinson <andrew.dickinson@t2.local>
2017-04-14 17:34:33 -0700

    Still non-functioning router

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {Router,Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import Signup from './Signup';
 import About from './About';
 require('../../scss/style.scss');
 
+const history = syncHistoryWithStore(browserHistory, store)
+
 const App = () => (
-  <Provider >
     <Router history={ browserHistory } >
       <Route path="/" component={Signup} />
       <Route path='/about' component={About} />
     </Router>
-  </Provider>
 );
 
 
