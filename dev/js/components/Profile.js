455b9412668ca294749bce77f1e4b43e043037c3
Robin Choi <robinchoii@gmail.com>
2017-04-14 19:08:59 -0700

    update profile skeleton

--- dev/js/components/Profile.js
+++ dev/js/components/Profile.js
@@ -6,19 +6,23 @@ class Profile extends React.Component{
     <div className="profile">
       <h2>CHECK IN</h2>
 
-      <img className="img-rounded" src={this.props.user.picture}>
+      <img className="img-rounded" src={this.props.user.img}>
       <p>Name: {this.props.user.first} {this.props.user.last} </p>
       <p>Location: {this.props.user.location}
-      <p>{this.props.numberOfVisits} Total Visits</p>
+      <p>{this.props.total} Total Visits</p>
+
       <process bar to goal>
+
       <div className="badge">
         <p>most visited park </p>
         <img>
       </div>
+
       <div className="badge">
         <p>number of different parks visited</p>
         <img>
       </div>
+
       <div className="badge">
         <p>days visited this year</p>
         <img>
